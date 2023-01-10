cefa24d7e4ddaad8075e7bb7a6452ca018dc0bc4
UltraMKF12 <harcos212@gmail.com>
2023-01-10 19:37:16 +0200

    Make fullscreen and exit button options

--- Prototypes/001/Scenes/Globals.gd
+++ Prototypes/001/Scenes/Globals.gd
@@ -1,4 +1,10 @@
 extends Node
 
 var number_of_bullets = 5
-var player_lives = 3
+var player_lives = 4
+
+func _process(delta):
+	if Input.is_action_just_pressed("fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+	elif Input.is_action_just_pressed("exit"):
+		get_tree().quit()
